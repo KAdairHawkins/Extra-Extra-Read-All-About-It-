97d2e20765088316218f1015453b1ed07edca19e
Adair Hawkins <adairhawkins@gmail.com>
2017-12-01 16:51:38 -0600

    created basic framework for server and routing
    created server routing to controller, added more node modules, public and views folders, and configued heroku to git repo

--- server.js
+++ server.js
@@ -1,24 +1,61 @@
-const express = require("express");
-const methodOverride = require("method-override");
-const bodyParser = require("body-parser");
-const exphbs = require('express-handlebars');
-const path = require('path');
+//Declare all my requirements. On one line, kinda, because I'm stylish like that.
+const   express = require("express"),
+        methodOverride = require("method-override"),
+        bodyParser = require("body-parser"),
+        exphbs = require('express-handlebars'),
+        app = express(),
+        mongoose = require('mongoose')
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
+
+// Set a route to serve static files (HTML, CSS, images, etc)
 app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
+//Allow Mongoose to use ES6 Promises.
+ mongoose.Promise = Promise;
+// //If running on Heroku, connect using that config variable.
+ if(process.env.MONGODB_URI){
+     mongoose.connect(process.env.MONGODB_URI,
+     {
+         useMongoClient: true
+     },
+     function(){
+//         //Callback function to make sure that things are working in Prod.
+         console.log("Connected in Production Environment");
+     });
+ }
+//If running locally, connect using that same URL so I just have to care about one DB, but hidden in a config file that's included in my .gitignore file.
+//No using my Database for nefarious purposes. :-p
+// else {
+//     const devURI = require("./config/dev.js");
+//     mongoose.connect(devURI,
+//     {
+//         useMongoClient: true
+//     },
+//     function(){
+//         //Callback function to make sure that things are working in Dev.
+//         console.log("Connected in Development environment");
+//     });
+// }
+
+//Set up Handlebars to use later
 // app.use(methodOverride('_method'));
 // app.engine('handlebars', exphbs({
 //     defaultLayout: 'main'
 // }));
-// app.set('view engine', 'handlebars');
-// var routes = require('./controllers/controller.js');
-// app.use('/', routes);
 
+
+//app.set('view engine', 'handlebars');
+const routes = require('./controller/controller.js');
+app.use('/', routes);
+
+var PORT = 3001;
 app.listen(process.env.PORT || 3001, function(){
-  console.log("app listening on on " + process.env.PORT || 3001);
+    if (process.env.PORT){
+        console.log(`App listening on ${process.env.PORT}`);
+    } else {
+        console.log(`App listening on 3001`);
+    }
 });
