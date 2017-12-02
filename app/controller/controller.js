c3d664063917bb64ddb306a710d156254e9c44d0
Adair Hawkins <adairhawkins@gmail.com>
2017-12-02 16:27:20 -0600

    updated router
    created redirect link

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-  res.redirect("/home");
+  res.render("home");
 });
 
 router.get('/home', function(req,res){
