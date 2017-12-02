c15ea4b7ad9cd0e2d507b150dad27248cf03b03a
Adair Hawkins <adairhawkins@gmail.com>
2017-12-02 15:47:05 -0600

    fixed router errors
    corrected router typos

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -2,11 +2,11 @@ const express = require ('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-  res.send("What's up?");
+  res.send("home", {});
 });
 
 router.get('/home', function(req,res){
-    res.redirect("home");
+    res.redirect("home", {});
 });
 
 router.get('/saved', function(req,res){
